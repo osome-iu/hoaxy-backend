5dc879c80c61d7d1370f52f2dac1787807a12695
Chengcheng Shao <shaoc@indiana.edu>
2018-08-07 23:34:31 +0800

    Add new command to load tweets from gzfile

--- hoaxy/commands/sns.py
+++ hoaxy/commands/sns.py
@@ -7,20 +7,23 @@ implemented.
 #
 # written by Chengcheng Shao <sccotte@gmail.com>
 
+import gzip
+import logging
+import Queue
+import smtplib
+import time
+
+import simplejson as json
+
 from hoaxy.commands import HoaxyCommand
 from hoaxy.database import Session
-from hoaxy.database.functions import get_platform_id
-from hoaxy.database.functions import get_site_tuples
+from hoaxy.database.functions import get_platform_id, get_site_tuples
 from hoaxy.database.models import N_PLATFORM_TWITTER
 from hoaxy.sns.twitter.handlers import QueueHandler
-from hoaxy.sns.twitter.parsers import QueueParser
+from hoaxy.sns.twitter.parsers import BulkParser, QueueParser
 from hoaxy.sns.twitter.stream import TwitterStream
 from hoaxy.utils import get_track_keywords
 from hoaxy.utils.log import configure_logging
-import Queue
-import logging
-import smtplib
-import time
 
 logger = logging.getLogger(__name__)
 
@@ -30,18 +33,21 @@ class SNS(HoaxyCommand):
 usage:
   hoaxy sns --twitter-streaming
             [(--mail-from=<f> --mail-to=<t>) --mail-server=<s>]
+  hoaxy sns --twitter-load-gzfile --gzfile=<gf>
   hoaxy sns -h | --help
 
 Track posted messages in social networks. Right now only twitter platform is
 implemented.
 
 --twitter-streaming     Start twitter streaming.
+--twitter-load-gzfile   Load tweets store as gzip file. Each line is a tweet.
 --mail-server=<s>       SMTP server host address. When process exits
                           unexceptionally, hoaxy send you email.
                           [default: localhost]
 --mail-from=<f>         From user address, when sending email.
 --mail-to=<t>           To user address, when sending email.
--h --help           Show help.
+--gzfile=<gf>           The name of the gzip file.
+-h --help               Show help.
 
 Since twitter streaming is a long running process. It is good to monitor this
 process, especially when process exited because of exceptions. We provide
@@ -113,6 +119,36 @@ Examples:
         logger.info('Exit')
 
     @classmethod
+    def twitter_load_gzfile(cls, gzfile, session):
+        snut = cls.conf['sns']['twitter']['save_none_url_tweet']
+        platform_id = get_platform_id(session, name=N_PLATFORM_TWITTER)
+        parser = BulkParser(platform_id=platform_id, save_none_url_tweet=snut)
+        with gzip.open(gzfile, 'r') as f:
+            g_urls_map = dict()
+            g_uusers_set = set()
+            g_edges_set = set()
+            i = 0
+            for line in f:
+                try:
+                    jd = json.loads(line, encoding='utf-8')
+                except Exception as e:
+                    logger.error(e)
+                    continue
+                parser.parse_new_one(
+                    jd,
+                    session,
+                    g_urls_map=g_urls_map,
+                    g_uusers_set=g_uusers_set,
+                    g_edges_set=g_edges_set)
+                i = i + 1
+                if i%10000 == 0:
+                    logger.info('Counter %s', i)
+                    parser.save_bulk(session, g_uusers_set, g_edges_set)
+                    g_urls_map = dict()
+                    g_uusers_set = set()
+                    g_edges_set = set()
+
+    @classmethod
     def run(cls, args):
         """Overriding method as the entry point of this command."""
         session = Session(expire_on_commit=False)
@@ -120,3 +156,8 @@ Examples:
             configure_logging(
                 'twitter.streaming', console_level=args['--console-log-level'])
             cls.twitter_stream(session, args)
+
+        if args['--twitter-load-gzfile'] is True:
+            configure_logging(
+                'twitter.load-gzfile', console_level=args['--console-log-level'])
+            cls.twitter_load_gzfile(args['--gzfile'], session)
