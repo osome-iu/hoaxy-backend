c7725221f36e1204d9b9e247770226274961d14d
chathuriw <kamalasini@gmail.com>
2019-09-04 09:52:30 -0400

    RapidAPI key parameter on botometer

--- hoaxy/commands/report.py
+++ hoaxy/commands/report.py
@@ -81,7 +81,7 @@ is working.
         """
         logger.info('Fetching bot score for top spreaders ...')
         bon = botometer.Botometer(
-            rapid_key=cls.conf['botometer']['rapid_key'],
+            rapidapi_key=cls.conf['botometer']['rapid_key'],
             wait_on_ratelimit=True,
             **cls.conf['botometer']['twitter_app_credentials'])
         max_retries = 3
