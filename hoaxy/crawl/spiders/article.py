a993dfed895ec63782c7ff6b8ee5fd16fee78526
cpelikan <cpelikan@iu.edu>
2021-10-28 13:48:05 -0400

    ignore top image when parse through newspaper3k

--- hoaxy/crawl/spiders/article.py
+++ hoaxy/crawl/spiders/article.py
@@ -168,7 +168,6 @@ is not determined""", url_id)
             try:
                 # First use Mercury
                 try:
-
                     canonical_url = item['canonical_url']
                     escaped_url = quote(canonical_url, safe='/:?=&')
                     if canonical_url is not None and canonical_url != "":
@@ -206,6 +205,7 @@ is not determined""", url_id)
                         newspaper_article = npArticle(url='')
                         if data['html']:
                             newspaper_article.set_html(data['html'])
+                            newspaper_article.set_top_img(None)
                         else:
                             raise Exception('Newspaper returned null content.')
                         newspaper_article.parse()
