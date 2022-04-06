b5d085b5b1c468ff04b8b37266a1b8c731fb5e93
Vijay Lulla <vlulla@iu.edu>
2022-04-06 08:39:57 -0400

    reverting back to previous state...

--- hoaxy/sns/twitter/handlers.py
+++ hoaxy/sns/twitter/handlers.py
@@ -372,7 +372,7 @@ class QueueHandler(BaseHandler):
                     self.is_connection_failed = True
                 else:
                     logger.error(err)
-                    ## self.on_db_bulk_save_error() ## TODO(vijay): see if commenting this stops dumping the tweets in the dumps folder
+                    self.on_db_bulk_save_error()
             except SQLAlchemyError as err:
                 session.rollback()
                 logger.exception(err)
